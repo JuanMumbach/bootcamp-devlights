bc173293e18f9df96874eeab5bcf711cefbd48a1
juanmumbach <juanimumbach@gmail.com>
2024-07-27 20:23:15 -0300

    Cambio nombre archivos actividad 1

--- dotnet/actividad1/obj/Debug/net8.0/actividad1.AssemblyInfo.cs
+++ dotnet/actividad1/obj/Debug/net8.0/actividad1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("bootcamp-devlights-dotnet")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("actividad1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8dd49aa66210900f8465ff3b068e3314a0ee753")]
-[assembly: System.Reflection.AssemblyProductAttribute("bootcamp-devlights-dotnet")]
-[assembly: System.Reflection.AssemblyTitleAttribute("bootcamp-devlights-dotnet")]
+[assembly: System.Reflection.AssemblyProductAttribute("actividad1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("actividad1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
