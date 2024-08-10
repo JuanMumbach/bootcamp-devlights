7536efb1dc9141bbd7219b3401ae0bbcc4b2e251
juanmumbach <juanimumbach@gmail.com>
2024-08-10 20:37:29 -0300

    Añadida la solucion de la actividad obligatoria del modulo 2

---
+++ dotnet/ActividadModulo2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ActividadModulo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
