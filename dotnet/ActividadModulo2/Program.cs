4644a3573a1e0e0f38c4c372a2f0a9a2a8c87625
juanmumbach <juanimumbach@gmail.com>
2024-08-11 22:16:30 -0300

    Actividad modulo 2 terminada

--- dotnet/ActividadModulo2/Program.cs
+++ dotnet/ActividadModulo2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Net.NetworkInformation;
 
 namespace ActividadModulo2
 {
@@ -9,7 +10,8 @@ namespace ActividadModulo2
 
         static float[] promedioSemanas = new float[5];
 
-        static List<float> sobreUmbral = new List<float>();
+        static List<float> temperaturaSobreUmbral = new List<float>();
+        static List<int> fechaSobreUmbral = new List<int>();
 
         static float umbral = 20;
 
@@ -107,41 +109,154 @@ namespace ActividadModulo2
         /*Funciones Opciones*/
         static void ConsultarTemperaturaDia() 
         {
-        
+            Console.WriteLine("Elige la fecha de la que quieres consultar la temperatura (un numero entero del 1 al 31):");
+            if (int.TryParse(Console.ReadLine(),out int diaConsulta))
+            {
+                int semana = (diaConsulta-1) / 7;
+                int dia = (diaConsulta - 1) - ((diaConsulta - 1) / 7) * 7;
+                int temperatura = temperaturasDias[semana,dia];
+                Console.WriteLine($"Semana {semana+1} dia {dia+1}");
+                Console.WriteLine($"La temperatura del dia {diaConsulta} es de {temperatura} grados.");
+            }
         }
 
         static void MostrarTemperaturasPromedioSemanales()
         {
             CalcularTemperaturasPromedio();
+
+            Console.WriteLine("La temperatura promedio de las semanas es:");
+            for (int i = 0; i < promedioSemanas.Length; i++)
+            {
+                Console.WriteLine($"Semana {(i + 1)}: {promedioSemanas[i]} grados.");
+            }
         }
 
         static void ConsultarTemperaturaPromedioMes()
         {
+            int sumatoria = 0;
+            for (int i = 0; i < temperaturasDias.GetLength(0); i++)
+            {
+                for (int j = 0; j < temperaturasDias.GetLength(1); j++)
+                {
+                    if (((i + 1) * 7 + (j + 1)) > 31) break;
+                    sumatoria += temperaturasDias[i, j];
+                }
 
+            }
+
+            float promedio = sumatoria / 31;
+
+            Console.WriteLine($"La temperatura promedio del mes es de {promedio} grados.");
         }
 
         static void MostrarTemperaturasSobreUmbral()
         {
             BuscarTemperaturasSobreUmbral();
+
+            Console.WriteLine($"Los dias en los que la temperatura supero los {umbral} grados son:");
+            
+            if (fechaSobreUmbral.Count == 0)
+            {
+                Console.WriteLine($"No hay dias que superen el umbral de {umbral} grados.");
+                return;
+            }
+
+            for (int i = 0; i < fechaSobreUmbral.Count; i++)
+            {
+                Console.WriteLine($"Dia {fechaSobreUmbral[i]}: {temperaturaSobreUmbral[i]} grados.");
+            }
         }
 
         static void ConsultarTemperaturaMasAlta()
         {
+            float temperaturaAlta = temperaturasDias[0,0];
+            int fechaTempAlta = 1;
+
+            for (int i = 0; i < temperaturasDias.GetLength(0); i++)
+            {
+                for (int j = 0; j < temperaturasDias.GetLength(1); j++)
+                {
+                    if (((i + 1) * 7 + (j + 1)) > 31) break;
+                    
+                    if (temperaturasDias[i, j] > temperaturaAlta)
+                    {
+                        temperaturaAlta = temperaturasDias[i, j];
+                        fechaTempAlta = ((i + 1) * 7 + (j + 1));
+                    }
+                }
+            }
+
+            Console.WriteLine($"El dia con la temperatura mas alta es el dia {fechaTempAlta} con una temperatura de {temperaturaAlta} grados.");
         }
 
         static void ConsultarTemperaturaMasBaja()
         {
+            float temperaturaBaja = temperaturasDias[0, 0];
+            int fechaTempBaja = 1;
+            for (int i = 0; i < temperaturasDias.GetLength(0); i++)
+            {
+                for (int j = 0; j < temperaturasDias.GetLength(1); j++)
+                {
+                    if (((i + 1) * 7 + (j + 1)) > 31) break;
+
+                    if (temperaturasDias[i, j] < temperaturaBaja)
+                    {
+                        temperaturaBaja = temperaturasDias[i, j];
+                        fechaTempBaja = ((i + 1) * 7 + (j + 1));
+                    }
+                }
+            }
+
+            Console.WriteLine($"El dia con la temperatura mas alta es el dia {fechaTempBaja} con una temperatura de {temperaturaBaja} grados.");
+
         }
 
         static void Consultar5diasSiguienteMes()
         {
+            Console.WriteLine("Las temperaturas de los primeros 5 dias del mes siguiente son:");
+            for(int i = 2;i < 7;i++)
+            {
+                Console.WriteLine($"Dia {i-1}: {temperaturasDias[4,i]} grados.");
+            }
         }
         #endregion
 
         #region Funciones Auxiliares
-        static void CalcularTemperaturasPromedio() { }
+        static void CalcularTemperaturasPromedio()
+        {
+            for (int i = 0; i < temperaturasDias.GetLength(0); i++)
+            {
+                float sumatoria = 0;
+                for(int j = 0; j < temperaturasDias.GetLength(1);j++)
+                {
+                    sumatoria += temperaturasDias[i, j];
+                }
+                promedioSemanas[i] = sumatoria / temperaturasDias.GetLength(1);
+            }
+        }
 
-        static void BuscarTemperaturasSobreUmbral() { }
+        static void BuscarTemperaturasSobreUmbral()
+        {
+            fechaSobreUmbral.Clear();
+            temperaturaSobreUmbral.Clear();
+            
+            for (int i = 0; i < temperaturasDias.GetLength(0);i++)
+            {
+                for (int j = 0; j < temperaturasDias.GetLength(1); j++)
+                {
+                    if (((i + 1) * 7 + (j + 1)) > 31) break;
+
+
+                    if (temperaturasDias[i,j] > umbral)
+                    {
+                        fechaSobreUmbral.Add((i + 1) * 7 + (j + 1));
+                        temperaturaSobreUmbral.Add(temperaturasDias[i, j]);
+                    }
+                }
+
+            }
+
+        }
 
         #endregion
     }
--- dotnet/ActividadModulo2/obj/Debug/net8.0/ActividadModulo2.AssemblyInfo.cs
+++ dotnet/ActividadModulo2/obj/Debug/net8.0/ActividadModulo2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActividadModulo2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7536efb1dc9141bbd7219b3401ae0bbcc4b2e251")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69922daba6f5da863595dadfa42c2b9e4a6efde")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActividadModulo2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActividadModulo2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
